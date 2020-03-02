0e4c92a1460df71c315b7e1f600d4e7a9a29a933
James <james.s.shoaf@gmail.com>
2020-03-02 11:04:35 -0800

    implement boardSolver functions

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -32,21 +32,80 @@ var model = {
         //render the victory banner based on the string
         view.renderBanner(victory[1]);
       }
-    }
     //regardless, pass the new board to the render function
     view.renderBoard(newBoard);
   },
 
 
     //send that board to View's render functions
+  solverHelpers: {
+    //diagonal helper functions
+    majorDiagonalHelper: (board) => {
+      if (board[0][0] === 'X' && board[1][1] === 'X' && board[2][2] === 'X') {
+        return 'X';
+      } else if (board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] === 'O') {
+        return 'O';
+      } else {
+        return false;
+      }
+    },
+
+    minorDiagonalHelper: (board) => {
+      if (board[2][0] === 'X' && board[1][1] === 'X' && board[0][2] === 'X') {
+        return 'X';
+      } else if (board[2][0] === 'O' && board[1][1] === 'O' && board[0][2] === 'O') {
+        return 'O';
+      } else {
+        return false;
+      }
+    },
+
+    //row helper function
+    rowHelper: (board) => {
+      for (let i = 0; i < 3; i++) {
+        if (board[i][0] === 'X' && board[i][1] === 'X' && board[i][2] === 'X') {
+          return 'X'
+        } else if (board[i][0] === 'O' && board[i][1] === 'O' && board[i][2] === 'O') {
+          return 'O'
+        }
+      }
+      return false;
+    },
+
+    //column helper functions
+    columnHelper: (board) => {
+      for (let i = 0; i < 3; i++) {
+        if (board[0][i] === 'X' && board[1][i] === 'X' && board[2][i] === 'X') {
+          return 'X'
+        } else if (board[0][i] === 'O' && board[1][i] === 'O' && board[2][i] === 'O') {
+          return 'O'
+        }
+      }
+      return false;
+    }
+  },
 
   //check if board has been solved
   isBoardSolved: (board) => {
-
-  }
-    //diagonal helper functions
-    //row helper functions
-    //column helper functions
+    //check each helper function in increasing order of time complexity
+    var solved = majorDiagonalHelper(board);
+    if (!solved) {
+      solved = minorDiagonalHelper(board);
+    }
+    if (!solved) {
+      solved = rowHelper(board);
+    }
+    if (!solved) {
+      solved = columnHelper(board);
+    }
+    //if solved is still not true, the game continues!
+    if (!solved) {
+      //return false and null
+      return [solved, null];
+    } else {
+      //if the board is solved, return true and the winner
+      return [true, solved];
+    }
     /* premature optimization - check for games where victory is impossible */
 }
 
